30d520be72d5de8a5485fcf538899927d57c4bda
Eliadad <eliasashtonsaunders@gmail.com>
2021-04-22 20:46:20 -0700

    added username functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,28 @@
-var inputForm = $('#input-form');
+var inputForm = document.querySelector('#input-form');
+var userSec = document.querySelector('#user-sec');
+var nameInput = document.querySelector('#name-input');
+var submit = document.querySelector('#sbmt-btn');
 
 
-var genderButton
+var userArr = ["inator", "killer", "cutter", "swagger", "michael", "robot", "droid", "wizard", "cop", "raptor", "flower", "sun", "space", "train"];
+
+var submitted = function(event) {
+
+    event.preventDefault();
 
 
-var maleArr = ["inator", "killer", "cutter", "swagger", ];
+    var objDiv = document.createElement("div");
+    objDiv.setAttribute("id", "profile-div");
 
-var submitted = function() {
-    
+    userSec.append(objDiv);
+
+    var userFinish = userArr[Math.floor(Math.random() * userArr.length)];
+
+    var userText = document.createElement("h3")
+    userText.textContent = nameInput.value + userFinish;
+
+    objDiv.appendChild(userText);
+
 };
 
-inputForm.addEventListener("click", submitted);
+submit.addEventListener("click", submitted);
