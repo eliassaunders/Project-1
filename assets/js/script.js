23500f5f4bb3fbad7b37a618e72b3f99c7b5586b
Eliadad <eliasashtonsaunders@gmail.com>
2021-04-22 19:52:28 -0700

    some java now

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var inputForm = $('#input-form');
+
+
+var genderButton
+
+
+var maleArr = ["inator", "killer", "cutter", "swagger", ];
+
+var submitted = function() {
+    
+};
+
+inputForm.addEventListener("click", submitted);
