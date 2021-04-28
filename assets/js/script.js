09beff1d9269db575ab0991b012344b84042078a
Eliadad <eliasashtonsaunders@gmail.com>
2021-04-27 20:31:24 -0700

    wikipedia parts almost done

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var persStr = document.querySelector("#pers");
 
 var userArr = ["Inator", "Killer", "Cutter", "Swagger", "Michael", "Robot", "droid", "wizard", "raptor", "flower", "sun", "lover", "fighter", "hater"];
 
-var submitted = function(event) {
+var submitted = function (event, objDiv) {
 
     event.preventDefault();
 
@@ -27,40 +27,56 @@ var submitted = function(event) {
     userText.textContent = inputStr + userFinish;
 
     objDiv.appendChild(userText);
-   
+
     var searchStr = persStr.value + "+" + animalInput.value;
 
     console.log(searchStr)
-    
+
     fetch('https://api.giphy.com/v1/gifs/search?q=' + searchStr + '&api_key=2WCCtsAYzLPcXYXldVYiYd41E1x6jlfT')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (response) {
-        console.log(response.data[0]);
-        // Create a variable that will select the <div> where the GIF will be displayed 
-        
-        var imgContainter = document.createElement("div");
-        var profText = document.createElement("h3");
-        profText.textContent = "Your profile gif:"
-        objDiv.appendChild(profText);
-        objDiv.appendChild(imgContainter);
-      
-        // Empty out the <div> before we append a GIF to it
-        imgContainter.innerHTML = '';
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            console.log(response.data[0]);
+            // Create a variable that will select the <div> where the GIF will be displayed 
 
-        var gifImg = document.createElement('img');
-        gifImg.setAttribute("height", "200px");
-        gifImg.setAttribute("width", "200px");
-        gifImg.setAttribute('src', response.data[0].images.fixed_height.url);
+            var imgContainter = document.createElement("div");
+            var profText = document.createElement("h3");
+            profText.textContent = "Your profile gif:"
+            objDiv.appendChild(profText);
+            objDiv.appendChild(imgContainter);
 
-        // Append 'gifImg' to the <div>
-        imgContainter.appendChild(gifImg);  
-       
-    });
-   
+            // Empty out the <div> before we append a GIF to it
+            imgContainter.innerHTML = '';
+
+            var gifImg = document.createElement('img');
+            gifImg.setAttribute("height", "200px");
+            gifImg.setAttribute("width", "200px");
+            gifImg.setAttribute('src', response.data[0].images.fixed_height.url);
+
+            // Append 'gifImg' to the <div>
+            imgContainter.appendChild(gifImg);
+
+            var buttonG = document.createElement("button");
+            buttonG.textContent = "Give me an interest"
+            buttonG.setAttribute("class", "card-button")
+
+            fetch("https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=1&origin=*")
+                .then(function (response) {
+                    console.log(response);
+                    response.json().then(function (article) {
+                        var diplayEl = document.createElement("p");
+                        console.log(article);
+                        diplayEl.textContent = article.query.random[0].title;
+
+                        objDiv.appendChild(diplayEl);
+                    })
+
+                })
+
+        });
 };
 
-console.log(persStr.value);
+
 
 submit.addEventListener("click", submitted);
